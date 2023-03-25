7a97d8e435fe4edbc91982546cfd2435749c0db4
Dennis Musembi <dennismusembi344@gmail.com>
2023-03-25 13:49:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+
+const dodger=document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+
+
+function moveDodgerLeft() {
+    let leftNumbers=dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+}
+}
+
+function moveDodgerRight() {
+    let leftNumbers=dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+}
+}
+function moveDodgerUp() {
+    var upNumbers = dodger.style.bottom.replace('px', '');
+    var up = parseInt(upNumbers, 10)
+    console.log(up)
+    if (up >= 0) {
+      dodger.style.bottom = `${up + 1}px`
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if(event.key==="ArrowLeft"){
+        moveDodgerLeft()
+    }
+    if(event.key==="ArrowRight"){
+        moveDodgerRight()
+    }
+    if(event.key==="ArrowUp"){
+        moveDodgerUp()
+    }
+});
+
+
+
+
+
+
+
+
